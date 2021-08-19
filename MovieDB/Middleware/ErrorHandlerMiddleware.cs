d4770d480c16d03627f63e60c9933b3415718da6
Swaroop Sittula <swaroop.sittula@modak.com>
2021-08-19 14:55:32 +0530

    Updated ErrorHandler MiddleWare

--- MovieDB/Middleware/ErrorHandlerMiddleware.cs
+++ MovieDB/Middleware/ErrorHandlerMiddleware.cs
@@ -7,8 +7,14 @@ using System.Threading.Tasks;
 
 namespace MovieDB.Middleware
 {
+    /// <summary>
+    /// The Purpose of this MiddleWare is to handle Internal Server Errors only
+    /// </summary>
     public class ErrorHandlerMiddleware
     {
+        /// <summary>
+        /// Http Request Delegete, stores next in pipeline and passes context to next Middleware in Pipeline
+        /// </summary>
         private readonly RequestDelegate _next;
 
         public ErrorHandlerMiddleware(RequestDelegate next)
@@ -16,39 +22,38 @@ namespace MovieDB.Middleware
             _next = next;
         }
 
+        /// <summary>
+        /// Method that catches the Internal Server Error, if not sends HttpContext to next Delegete in Pipeline 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public async Task InvokeAsync(HttpContext context)
         {
             try
             {
                 await _next(context);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                await HandleExceptionAsync(context, ex);
+                await HandleExceptionAsync(context);
             }
         }
 
-        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        /// <summary>
+        /// Async method to handle the Internal Server error and produce a custom error response
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private static async Task HandleExceptionAsync(HttpContext context)
         {
-            try
+            var result = JsonConvert.SerializeObject(new ErrorResponse()
             {
-                var result = JsonConvert.DeserializeObject<ErrorResponse>(exception.Message);
-                context.Response.ContentType = Constants.Json;
-                context.Response.StatusCode = result.StatusCode;
-                await context.Response.WriteAsync(exception.Message);
-
-            }
-            catch(Exception ex)
-            {
-                var result = JsonConvert.SerializeObject(new ErrorResponse()
-                {
-                    ErrorMessage = "Internal Server Error!",
-                    StatusCode = 500
-                });
-                context.Response.ContentType = Constants.Json;
-                context.Response.StatusCode = 500;
-                await context.Response.WriteAsync(result);
-            }
+                ErrorMessage = "Internal Server Error!",
+                StatusCode = 500
+            });
+            context.Response.ContentType = Constants.Json;
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsync(result);
         }
     }
 
