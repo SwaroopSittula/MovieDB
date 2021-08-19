c6e54783f2c9b552a1512bead271afb73bdcd4b6
Swaroop Sittula <swaroop.sittula@modak.com>
2021-08-19 14:38:51 +0530

    Addded comments in DatabaseSettings.cs

--- MovieDB/Models/DatabaseSettings.cs
+++ MovieDB/Models/DatabaseSettings.cs
@@ -1,10 +1,24 @@
 ﻿
 namespace MovieDB.Models
 {
+    /// <summary>
+    /// Class used to store Database settings
+    /// </summary>
     public class DatabaseSettings
     {
+        /// <summary>
+        /// Collection Name that is used by this API
+        /// </summary>
         public string CollectionName { get; set; }
+
+        /// <summary>
+        /// Database name that is connected is stored in this property
+        /// </summary>
         public string DatabaseName { get; set; }
+
+        /// <summary>
+        /// Connection string used to connect to the Database
+        /// </summary>
         public string ConnectionString { get; set; }
     }
 }
