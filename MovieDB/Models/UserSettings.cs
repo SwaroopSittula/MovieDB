4656241aff4d929e4c5d034d80937f7a7bf95b79
Swaroop Sittula <swaroop.sittula@modak.com>
2021-08-19 14:19:35 +0530

    Updated XML comments in UserSettings

--- MovieDB/Models/UserSettings.cs
+++ MovieDB/Models/UserSettings.cs
@@ -3,11 +3,24 @@ using MovieDB.Helpers;
 
 namespace MovieDB.Models
 {
+    /// <summary>
+    /// To store UserSettings
+    /// </summary>
     public class UserSettings
     {
+        /// <summary>
+        /// Audig log settings are stored in this property
+        /// </summary>
         public AuditLog AuditLog { get; set; }
+
+        /// <summary>
+        /// Stores base path for Miniprofiler
+        /// </summary>
         public string  BasePath { get; set; }
 
+        /// <summary>
+        /// To switch Miniprofiler to on and off
+        /// </summary>
         public bool EnableMiniProfiler { get; set; }
     }
 }
