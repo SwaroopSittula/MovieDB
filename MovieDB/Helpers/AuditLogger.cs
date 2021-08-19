9f70136e533e1c5373d98546392830d2ce52ce05
Swaroop Sittula <swaroop.sittula@modak.com>
2021-08-19 11:31:24 +0530

    Updateds xml comments in Auditlogger.cs

--- MovieDB/Helpers/AuditLogger.cs
+++ MovieDB/Helpers/AuditLogger.cs
@@ -7,6 +7,14 @@ namespace MovieDB.Helpers
 {
     public static class AuditLogger
     {
+        /// <summary>
+        /// This Method is used to "log the request info" whose respose typically have PII (Personally Identifiable Information)
+        /// </summary>
+        /// <param name="transactionId"></param>
+        /// <param name="method"></param>
+        /// <param name="path"></param>
+        /// <param name="queryString"></param>
+        /// <param name="payload"></param>
         public static void RequestInfo(string transactionId, string method, string path, string queryString, string payload)
         {
             AuditMiddleware.Logger.Information(
@@ -15,6 +23,16 @@ namespace MovieDB.Helpers
                 ));
         }
 
+        /// <summary>
+        /// To log the PII(Personally Identifiable Information) resposne info
+        /// </summary>
+        /// <param name="transactionId"></param>
+        /// <param name="method"></param>
+        /// <param name="path"></param>
+        /// <param name="queryString"></param>
+        /// <param name="databaseName"></param>
+        /// <param name="collectionName"></param>
+        /// <param name="payload"></param>
         public static void ResponseInfo(string transactionId, string method, string path, string queryString, string databaseName, string collectionName, string payload)
         {
             AuditMiddleware.Logger.Information(
