a4ad285b9150947751548eef02559da221a526d8
Swaroop Sittula <swaroop.sittula@modak.com>
2021-08-19 11:21:05 +0530

    Updated Audit Log with xml comments

--- MovieDB/Helpers/AuditLog.cs
+++ MovieDB/Helpers/AuditLog.cs
@@ -1,17 +1,27 @@
-﻿
-
-namespace MovieDB.Helpers
+﻿namespace MovieDB.Helpers
 {
+    /// <summary>
+    /// Settings related to Audit Log are injected to the class properties
+    /// </summary>
     public class AuditLog
-    {
-        #region Local
-
+    { 
+        /// <summary>
+        /// Path Where audit logs are stored
+        /// </summary>
         public string  Path { get; set; }
 
+        /// <summary>
+        /// Time to roll the logs in log file
+        /// </summary>
         public string  RollingInterval { get; set; }
+        /// <summary>
+        /// true by default
+        /// </summary>
         public bool Shared { get; set; }
+        /// <summary>
+        /// No of files to retain in the logs
+        /// </summary>
         public int RetainedFileCountLimit { get; set; }
 
-        #endregion
     }
 }
