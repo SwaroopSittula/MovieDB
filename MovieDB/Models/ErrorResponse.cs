7a75c0bc2010ecc54e66db9f8da33c6756073686
Swaroop Sittula <swaroop.sittula@modak.com>
2021-08-19 14:36:29 +0530

    Updated comments in ErrorResponse.cs

--- MovieDB/Models/ErrorResponse.cs
+++ MovieDB/Models/ErrorResponse.cs
@@ -5,9 +5,18 @@ using System.Threading.Tasks;
 
 namespace MovieDB.Models
 {
+    /// <summary>
+    /// Error Response Standard that is followed for errors
+    /// </summary>
     public class ErrorResponse
     {
+        /// <summary>
+        /// Content related to Error in ErrorMessage
+        /// </summary>
         public string ErrorMessage { get; set; }
+        /// <summary>
+        /// Status code corresponding to the error occurred
+        /// </summary>
         public int StatusCode { get; set; }
     }
 }
