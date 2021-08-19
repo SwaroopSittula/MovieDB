d9efcf8e47fbcdea95eecd974f15986c4dda4b38
Swaroop Sittula <swaroop.sittula@modak.com>
2021-08-19 14:03:34 +0530

    Added XML comments to IMovieRepository

--- MovieDB/Repository/Interfaces/IMovieRepository.cs
+++ MovieDB/Repository/Interfaces/IMovieRepository.cs
@@ -8,7 +8,18 @@ namespace MovieDB.Repository.Interfaces
 {
     public interface IMovieRepository
     {
+        /// <summary>
+        /// This method is to retrieve Movie Info based on Id.
+        /// This method is to retrieve Movie Info based on Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<ActionResult> GetMovie(int id);
+
+        /// <summary>
+        /// Health check endpoint abstraction
+        /// </summary>
+        /// <returns></returns>
         public Task<bool> IsAliveAsync();
     }
 }
